a5b0b51175236239af6ced4f93d4003cecf805df
Giuseppe Valenti <giuseppevalenti@Giuseppes-MacBook-Pro-4.local>
2017-10-07 17:10:40 +0200

    0.1.02

---
+++ Example/Pods/Target Support Files/EmptyP0d/EmptyP0d-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EmptyP0d : NSObject
+@end
+@implementation PodsDummy_EmptyP0d
+@end
---
+++ Example/Pods/Target Support Files/Pods-EmptyP0d_Tests/Pods-EmptyP0d_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EmptyP0d_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_EmptyP0d_Tests
+@end
