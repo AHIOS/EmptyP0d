1a0d2790c802584ffa2e25ee4ac534eb143552e2
Giuseppe Valenti <giuseppevalenti@Giuseppes-MacBook-Pro-4.local>
2017-10-07 17:06:12 +0200

    Initial commit

---
+++ Example/EmptyP0d/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  EmptyP0d
+//
+//  Created by AHIOS on 10/07/2017.
+//  Copyright (c) 2017 AHIOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
